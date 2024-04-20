241e4ead99a0db484aa66c4a88e79800406c42ea
Rodrigo Barbosa <167621274+Rodrgou@users.noreply.github.com>
2024-04-20 09:47:29 -0300

    Add files via upload

---
+++ pescador.js
@@ -0,0 +1,12 @@
+function verificador(){
+
+    const p = parseFloat(document.getElementById("peso").value);
+
+    excesso = 0
+    mmulta = 0
+
+if (p > 50) {
+    excesso = p - 50;
+    multa = excesso * 4;
+    alert("O Peso "+ p +"Kg\nExcesso: "+ excesso +"Kg\nMulta por excesso de Kg: R$"+ multa);
+}}
